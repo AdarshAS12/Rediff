079c64b4697d87c129bc15383eca1673e96326c7
AdarshAS12 <99171476+AdarshAS12@users.noreply.github.com>
2025-08-09 21:45:46 +0530

    RediffProject.java

--- RediffProject.java
+++ RediffProject.java
@@ -2,7 +2,6 @@ package testngPgms;
 
 import java.io.File;
 import java.io.IOException;
-import java.time.Duration;
 import java.util.List;
 
 import org.testng.annotations.AfterTest;
@@ -15,9 +14,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.io.FileHandler;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class RediffProject {
 	ChromeDriver driver;
